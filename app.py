39b3b94f97534495f682192fbed6027a40646164
Luann Rangel <luannrangel2701@gmail.com>
2026-01-09 16:54:21 -0300

    Corrige porta para Render

--- app.py
+++ app.py
@@ -1080,10 +1080,10 @@ def reset_senha(token):
     conn.close()
     return render_template("reset_senha.html")
 
+# -----------------------
 
-# ======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
